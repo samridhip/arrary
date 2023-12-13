785b875638d2f80678cd668ddc315d37b374d9e0
samridhip <pandeysamridhi1@gmail.com>
2023-12-14 04:34:45 +0530

    Arrays practise

---
+++ src/Question1.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Question1 {
+    public static void main(String[]args){
+        int[] array_1={44,88,58,95,03,18};
+        String[] array_2= {"java","sam","house","boy","video" };
+        System.out.println("Original integer "+Arrays.toString(array_1));
+        System.out.println("Original String "+Arrays.toString(array_2));
+        Arrays.sort(array_1);
+        System.out.println("Sorted integer "+ Arrays.toString(array_1));
+        Arrays.sort(array_2);
+        System.out.println("Sorted String"+Arrays.toString(array_2));
+
+    }
+}
