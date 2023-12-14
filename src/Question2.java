0f435408a2d3c0acc5cf6294ecbf90c11f5ad833
samridhip <pandeysamridhi1@gmail.com>
2023-12-14 11:55:25 +0530

    Arrays practise

---
+++ src/Question2.java
@@ -0,0 +1,13 @@
+public class Question2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 6, 4, 8, 6, 9};
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            System.out.println("The sum of the array is" + sum);
+        }
+    }
+}
+
+
+
