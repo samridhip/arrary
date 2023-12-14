943594b3f5bce185120f1b145c41f775b5216fb8
samridhip <pandeysamridhi1@gmail.com>
2023-12-14 12:05:50 +0530

    Arrays practise

---
+++ src/Question3.java
@@ -0,0 +1,10 @@
+public class Question3 {
+    public static void main(String[]args){
+        int[][]a=new int[10][10];
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++)
+                System.out.println(a[i][j]);
+        }
+        System.out.println();
+    }
+}
---
+++ src/Question4.java
@@ -0,0 +1,11 @@
+public class Question4 {
+    public static void main(String[]args){
+        int []arr={1,2,3,4,5,6};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+i;
+        }
+        double average=sum/arr.length;
+        System.out.println(average);
+    }
+}
