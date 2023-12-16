4e6237a2593657fffc51538b931c976523854617
samridhip <pandeysamridhi1@gmail.com>
2023-12-17 00:13:50 +0530

    Arrays practise

---
+++ src/Question5.java
@@ -0,0 +1,13 @@
+public class Question5 {
+    public static boolean main(String[]args){
+        int []arr={1,2,3,4,5,6,7,8,9};
+
+        for(int i:arr){
+            if(i==5){
+                return true;
+            }
+        }
+        return false;
+    }
+    System;
+}
