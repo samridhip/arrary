ef697b61788c9786a86441970ddc1a46c34543e2
samridhip <pandeysamridhi1@gmail.com>
2023-12-21 01:27:02 +0530

    Arrays practise

--- src/Question6.java
+++ src/Question6.java
@@ -1,3 +1,12 @@
 public class Question6 {
-
+    public static void main(String[] args) {
+        int[] arr={1,7,4,99,40};
+        int index=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==99){
+                index=i;
+            }
+        }
+        System.out.println(index);
+    }
 }
